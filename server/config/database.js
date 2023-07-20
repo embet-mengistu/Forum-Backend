a8843bc9e71aefb5ba4fc281b16f0373c8e5c64f
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2023-07-20 14:13:13 -0400

    database deployed

--- server/config/database.js
+++ server/config/database.js
@@ -1,13 +1,17 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const pool = mysql.createPool({
-  socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.MYSQL_DB,
-  password: process.env.DB_PASS,
-  connectionLimit: 10,
-});
+// const pool = mysql.createPool({
+//   socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock",
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.MYSQL_DB,
+//   password: process.env.DB_PASS,
+//   connectionLimit: 10,
+// });
+
+// const pool = process.env.DATABASE_URL;
+const pool = mysql.createPool(process.env.DATABASE_URL);
 
 pool.getConnection(function (err, conn) {
   console.log("database connected");
@@ -26,8 +30,8 @@ let profile = `CREATE TABLE if not exists profile(
     user_id int not null,
     first_name varchar(255) not null,
     last_name varchar(255) not null,
-    PRIMARY KEY ( user_profile_id),
-    FOREIGN KEY (user_id) REFERENCES registration (user_id)
+    PRIMARY KEY ( user_profile_id)
+  
     )`;
 
 let question = `CREATE TABLE if not exists question(
@@ -39,8 +43,8 @@ let question = `CREATE TABLE if not exists question(
     post_id varchar(255) not null,
     user_id int not null,
     PRIMARY KEY (question_id),
-    UNIQUE KEY (post_id),
-    FOREIGN KEY (user_id) REFERENCES registration (user_id)
+    UNIQUE KEY (post_id)
+    
     )`;
 
 let answer = `CREATE TABLE if not exists answer(
@@ -49,9 +53,8 @@ let answer = `CREATE TABLE if not exists answer(
     answer_code_block varchar(255) ,
     user_id int not null,
     question_id int not null,
-    PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES registration (user_id),
-    FOREIGN KEY (question_id) REFERENCES question (question_id)
+    PRIMARY KEY (answer_id)
+   
     )`;
 
 pool.query(registration, (err, results) => {
